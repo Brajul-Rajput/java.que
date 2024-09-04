34aef692cdf6e54db494381d5d087af4678070a7
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2024-09-04 19:50:01 +0530

    Create linears.java

---
+++ linears.java
@@ -0,0 +1,17 @@
+public class linears {
+    static int linearstring(String nameltr,char target){
+
+for(int i=0;i<nameltr.length();i++){
+    if(nameltr.charAt(i)==target)
+    return i;
+}
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        String name="brajul";
+        char target='j';
+        int ans=linearstring(name,'j');
+        System.out.println(ans);
+    }
+}
